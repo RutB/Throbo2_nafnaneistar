19293d222d7ab6ced5e4a1143d74c509e18d67cc
OlafurjonHI <ojv1@hi.is>
2020-10-22 08:44:25 +0000

    viewliked breytingar

--- nafnaneistar/src/main/resources/static/js/viewliked.js
+++ nafnaneistar/src/main/resources/static/js/viewliked.js
@@ -1,50 +1,56 @@
-document.addEventListener('DOMContentLoaded', ()=>{
+document.addEventListener('DOMContentLoaded', () => {
 
     let tabs = document.querySelectorAll('.viewliked__tab');
-    for(const t of tabs) {
-        t.addEventListener('click',openWindow);
+    for (const t of tabs) {
+        t.addEventListener('click', openWindow);
     }
     initStarConversion();
 
     let rows = document.querySelectorAll('.gender__row');
-    rows.forEach(row =>{
-        row.addEventListener('mouseleave',(e) => {
-            starConvertRow(row)
-        });
-    })
+    rows.forEach(row =>row.addEventListener('mouseleave', starConvertRow));
 
-    let stars  = document.querySelectorAll('.gender__rankstar');
+
+    let stars = document.querySelectorAll('.gender__rankstar');
     stars.forEach(star => {
-        star.addEventListener('mouseenter',starStruck)
+        star.addEventListener('mouseenter', starStruck)
         star.addEventListener('click', updateRank)
     })
 
-    function updateRank(e){
+
+    let partnerOptions = document.querySelectorAll('.select__option');
+    partnerOptions.forEach(partner => partner.addEventListener('click',customSelect))
+
+
+    function customSelect(e){
+
+    }
+
+
+
+    function updateRank(e) {
         let grandpapa = e.target.parentNode.parentNode
         let parent = e.target.parentNode;
         let nametd = grandpapa.querySelector('.gender__name');
         let id = nametd.getAttribute('id');
         let currank = parent.classList[0]
         let stars = parent.querySelectorAll('.filled');
-        let rank = stars[stars.length-1].classList[4].split('-')[1];
+        let rank = stars[stars.length - 1].classList[4].split('-')[1];
         parent.classList.remove(currank)
         parent.classList.remove("gender__rank")
         parent.classList.add(`rank${rank}`);
         parent.classList.add('gender__rank')
-        sendUpdateRating(id,rank)
+        sendUpdateRating(id, rank)
 
     }
-    
-
-    function starStruck(e){
-        let parent  = e.target.parentNode;
+    function starStruck(e) {
+        let parent = e.target.parentNode;
         let rankclass = e.target.classList[4];
         let targetrank = parseInt(rankclass.split('-')[1])
         let starcluster = parent.querySelectorAll('.gender__rankstar');
         starcluster.forEach(cstar => {
             let cstarclass = cstar.classList[4];
             let cstarrank = parseInt(cstarclass.split('-')[1])
-            if(cstarrank <= targetrank){
+            if (cstarrank <= targetrank) {
                 cstar.classList.remove('far')
                 cstar.classList.remove(cstarclass)
                 cstar.classList.remove('empty')
@@ -63,15 +69,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
         })
 
     }
-
-    
-    function starConvertRow(row){
-        let rank = row.querySelector('.gender__rank')
+    function starConvertRow(e) {
+        let rank = e.target.querySelector('.gender__rank')
         let maxrank = parseInt(rank.classList[0].split('rank')[1]);
-        for(let i = 0; i < rank.children.length; i++){
+        for (let i = 0; i < rank.children.length; i++) {
             let childclass = rank.children[i].classList[4];
             let childrank = parseInt(childclass.split('-')[1])
-            if(maxrank == 0) {
+            if (maxrank == 0) {
                 rank.children[i].classList.remove('fas')
                 rank.children[i].classList.remove(childclass)
                 rank.children[i].classList.remove("filled")
@@ -79,13 +83,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
                 rank.children[i].classList.add('far')
                 rank.children[i].classList.add(childclass)
             }
-            if(childrank <= maxrank){
+            if (childrank <= maxrank) {
                 rank.children[i].classList.remove('far')
                 rank.children[i].classList.remove(childclass)
                 rank.children[i].classList.add('fas')
                 rank.children[i].classList.add(childclass)
             }
-            if(childrank > maxrank){
+            if (childrank > maxrank) {
                 rank.children[i].classList.remove('fas')
                 rank.children[i].classList.remove(childclass)
                 rank.children[i].classList.add('far')
@@ -95,14 +99,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
         }
     }
 
-    function initStarConversion(){
+    function initStarConversion() {
         let ranks = document.querySelectorAll('.gender__rank');
         ranks.forEach(rank => {
             let value = parseInt(rank.textContent);
-            while(rank.firstChild)
+            while (rank.firstChild)
                 rank.removeChild(rank.firstChild);
-            for(let i = 1; i < 6;i++){
-                if(i <= value)
+            for (let i = 1; i < 6; i++) {
+                if (i <= value)
                     rank.appendChild(createFilledStar(i))
                 else
                     rank.appendChild(createEmptyStar(i));
@@ -111,29 +115,29 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     }
 
-    function createEmptyStar(rank){
+    function createEmptyStar(rank) {
         let star = document.createElement('i');
         star.classList.add('far');
         star.classList.add('fa-star')
         star.classList.add('gender__rankstar')
         star.classList.add('empty')
-        if(rank)
+        if (rank)
             star.classList.add(`starrank-${rank}`)
         return star;
     }
-    function createFilledStar(rank){
+    function createFilledStar(rank) {
         let star = document.createElement('i');
         star.classList.add('fas');
         star.classList.add('fa-star')
         star.classList.add('gender__rankstar')
         star.classList.add('filled')
-        if(rank)
+        if (rank)
             star.classList.add(`starrank-${rank}`)
         return star;
     }
 
-    function openWindow(e){
-        if(!e.target.getAttribute('id').includes('tab')) return;
+    function openWindow(e) {
+        if (!e.target.getAttribute('id').includes('tab')) return;
         let tabno = e.target.getAttribute('id');
         tabs.forEach(tab => tab.classList.remove('--active'));
         e.target.classList.add('--active')
@@ -144,20 +148,18 @@ document.addEventListener('DOMContentLoaded', ()=>{
         document.getElementById(window).classList.add('viewliked__active')
     }
 
-    function sendUpdateRating(id,rating){
+    function sendUpdateRating(id, rating) {
         let url = `${window.location.origin}/viewliked/updaterating?id=${id}&rating=${rating}`;
         fetch(url)
-        .then((resp) => {
-          if (resp.status !== 200) {
-            console.log(`Error ${resp.text()}`);
-            return;
-          }
-          return resp.json();
-        })
-        .then((data) => {
-            console.log(data)
-        });
+            .then((resp) => {
+                if (resp.status !== 200) {
+                    console.log(`Error ${resp.text()}`);
+                    return;
+                }
+                return resp.json();
+            })
+            .then((data) => {
+                console.log(data)
+            });
     }
-
-
 })
