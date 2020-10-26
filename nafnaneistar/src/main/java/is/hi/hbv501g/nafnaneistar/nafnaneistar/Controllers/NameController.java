bf721614ded144f717c9fa0c2745526bb13839fc
Hafthorat <sterinn@internet.is>
2020-10-26 13:59:12 +0000

    Searchname komin með leitarvirkni, skilar töflu á searchname.html

--- nafnaneistar/src/main/java/is/hi/hbv501g/nafnaneistar/nafnaneistar/Controllers/NameController.java
+++ nafnaneistar/src/main/java/is/hi/hbv501g/nafnaneistar/nafnaneistar/Controllers/NameController.java
@@ -3,7 +3,7 @@ package is.hi.hbv501g.nafnaneistar.nafnaneistar.Controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Optional;
-
+import org.apache.commons.lang3.StringUtils;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +17,8 @@ import is.hi.hbv501g.nafnaneistar.nafnaneistar.Entities.User;
 import is.hi.hbv501g.nafnaneistar.nafnaneistar.Services.NameService;
 import is.hi.hbv501g.nafnaneistar.nafnaneistar.Services.UserService;
 import is.hi.hbv501g.nafnaneistar.utils.UserUtils;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 /**
  * NameController
@@ -112,19 +114,35 @@ public class NameController {
         return "viewliked";
     }
 
-    // HAT
+    /**
+     * 
+     * @param model 
+     * 
+     * 
+     */
     @RequestMapping(value = "/searchname", method = RequestMethod.GET)
     public String searchName(Model model, HttpSession session) {
         User currentUser = (User) session.getAttribute("currentUser");
         if(!UserUtils.isLoggedIn(currentUser))
             return "redirect:/login";
         model.addAttribute("user", currentUser);
-        //model.addAttribute("names", nameService.findAllByNameLike(s)); // Þyrfti að vera POST fyrir þetta
-        //model.addAttribute("names", nameService.findAll());
-        ArrayList<NameCard> tempList = (ArrayList<NameCard>) nameService.findAllByNameLike("Sigur");
-        //ArrayList<NameCard> tempList = (ArrayList<NameCard>) nameService.findAll();
-        System.out.print(tempList.get(0).getName());
-        //System.out.println(nameService.findAllByNameLike("sigur"));
+
+        return "searchname";
+    }
+
+    @RequestMapping(value="/searchname", method = RequestMethod.POST)
+    public String searchName(@RequestParam(value = "searchedName", required = true)
+            String searchedName, Model model, HttpSession session) {
+        User currentUser = (User) session.getAttribute("currentUser");
+        if(!UserUtils.isLoggedIn(currentUser)){
+            return "redirect:login";
+        }
+        String s = searchedName.concat("%");
+        s = StringUtils.capitalize(s);
+        ArrayList<NameCard> SearchedList = (ArrayList<NameCard>) nameService.findAllByNameLike(s);
+        System.out.println(SearchedList.get(0).getName());
+        model.addAttribute("names", SearchedList);
+        
         return "searchname";
     }
 
