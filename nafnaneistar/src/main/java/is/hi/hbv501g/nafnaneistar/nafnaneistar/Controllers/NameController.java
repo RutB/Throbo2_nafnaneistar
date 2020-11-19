628b5892903e98e0f3f98fa24f37899155992dd6
Hafthorat <sterinn@internet.is>
2020-11-19 11:58:44 +0000

    Merge?

--- nafnaneistar/src/main/java/is/hi/hbv501g/nafnaneistar/nafnaneistar/Controllers/NameController.java
+++ nafnaneistar/src/main/java/is/hi/hbv501g/nafnaneistar/nafnaneistar/Controllers/NameController.java
@@ -148,7 +148,6 @@ public class NameController {
         if(!UserUtils.isLoggedIn(currentUser)){
             return "redirect:login";
         }
-<<<<<<< HEAD
         ArrayList<NameCard> searchedList;
         if ((Integer.parseInt(gender) == 3)) {
             searchedList = (ArrayList<NameCard>) nameService.findAllByNameLike(StringUtils.capitalize(searchedName.concat("%")));
@@ -158,16 +157,6 @@ public class NameController {
             searchedList = (ArrayList<NameCard>) nameService.findAllByNameLikeAndGender((StringUtils.capitalize(searchedName.concat("%"))), kyn);
         }
         model.addAttribute("names", searchedList);
-=======
-        model.addAttribute("user", currentUser);
-        
-        String s = searchedName.concat("%");
-        s = StringUtils.capitalize(s);
-        ArrayList<NameCard> SearchedList = (ArrayList<NameCard>) nameService.findAllByNameLike(s);
-        System.out.println(SearchedList.get(0).getName());
-        model.addAttribute("names", SearchedList);
-        
->>>>>>> master
         return "searchname";
     }
 }
